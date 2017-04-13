1421ef9bece3d7e9692487734fd7d44bceada923
kustrun <domen.balantic1@gmail.com>
2017-04-13 21:06:10 +0200

    Update settings.py

--- TPO_Patronazna_sluzba/settings.py
+++ TPO_Patronazna_sluzba/settings.py
@@ -131,6 +131,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+LANG='en_US.UTF-8'
+LC_ALL='en_US.UTF-8'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
