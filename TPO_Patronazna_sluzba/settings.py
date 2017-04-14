6f52a6c1a3c7761dda8290460ac514896f2cec21
elf2 <david.senica95@gmail.com>
2017-04-14 11:55:45 +0200

    Zacasno brisem podatke o emailu

--- TPO_Patronazna_sluzba/settings.py
+++ TPO_Patronazna_sluzba/settings.py
@@ -139,9 +139,4 @@ STATIC_ROOT = 'static'
 
 
 #Email konfiguration
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'testko.test2@gmail.com'
-EMAIL_HOST_PASSWORD = 'geslo1234'
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
